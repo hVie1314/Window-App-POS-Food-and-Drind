607127d40ccb3ef5830b5ce481aaa549e3ce2fb8
hVie1314 <88617436+hVie1314@users.noreply.github.com>
2024-10-15 22:02:06 +0700

    Add files via upload
    base cơ bản của chương trình, mn có thể clone về

---
+++ POS/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace POS.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private StorageFolder ? _inputFolder;
+
+    [ObservableProperty]
+    private StorageFolder? _outputFolder;
+
+    public MainWindowViewModel()
+    {
+
+    }
+}
+
